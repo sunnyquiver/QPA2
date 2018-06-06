sunnyquiver/QPA2

commit 1ea55cb6bdbca1f49e01771ce75333da9ed1098f

Some tweaking of a few methods to make them a little less inefficient.

lib/algebra.gi
@@ -1076,7 +1076,8 @@ function( paths, e )
   Ps := Paths( e );
   Cs := Coefficients( e );
   zero := Zero( LeftActingDomain( AlgebraOfElement( e ) ) );
-  coeffs := List( paths, p -> zero );
+  #coeffs := List( paths, p -> zero );
+  coeffs := [];
   j := Length( Ps );
   for i in [ 1 .. Length( paths ) ] do
     if j = 0 then
@@ -1085,7 +1086,13 @@ function( paths, e )
     if paths[ i ] = Ps[ j ] then
       coeffs[ i ] := Cs[ j ];
       j := j - 1;
+    else
+      coeffs[ i ] := zero;
     fi;
+  od;
+  while i <= Length( paths ) do
+    coeffs[ i ] := zero;
+    i := i + 1;
   od;
   return coeffs;
 end );

lib/quiver.gi
@@ -7,7 +7,7 @@ DeclareRepresentation( "IsVertexRep", IsComponentObjectRep and IsAttributeStorin
                        [ "quiver", "number" ] );
 DeclareRepresentation( "IsArrowRep", IsComponentObjectRep and IsAttributeStoringRep,
                        [ "quiver", "label", "number", "source", "target" ] );
-DeclareRepresentation( "IsCompositePathRep", IsComponentObjectRep,
+DeclareRepresentation( "IsCompositePathRep", IsComponentObjectRep and IsAttributeStoringRep,
                        [ "arrows" ] );
 DeclareRepresentation( "IsQuiverRep", IsComponentObjectRep and IsAttributeStoringRep,
 		       [ "label", "vertices", "arrows", "primitive_paths",
@@ -1294,28 +1294,28 @@ function( Q )
   return Q!.label;
 end );
 
-InstallMethod( \=, "for vertices",
- 	       [ IsVertex and IsVertexRep, IsVertex and IsVertexRep ],
-function( v1, v2 )
-  return QuiverOfPath( v1 ) = QuiverOfPath( v2 )
-         and VertexNumber( v1 ) = VertexNumber( v2 );
-end );
-
-InstallMethod( \=,
-               "for arrows",
- 	       [ IsArrow and IsArrowRep, IsArrow and IsArrowRep ],
-function( a1, a2 )
-  return QuiverOfPath( a1 ) = QuiverOfPath( a2 )
-         and ArrowNumber( a1 ) = ArrowNumber( a2 );
-end );
-
 # InstallMethod( \=, "for vertices",
-#                [ IsVertex and IsVertexRep, IsVertex and IsVertexRep ],
-#                IsIdenticalObj );
+#  	       [ IsVertex and IsVertexRep, IsVertex and IsVertexRep ],
+# function( v1, v2 )
+#   return QuiverOfPath( v1 ) = QuiverOfPath( v2 )
+#          and VertexNumber( v1 ) = VertexNumber( v2 );
+# end );
 
-# InstallMethod( \=, "for arrows",
-#                [ IsArrow and IsArrowRep, IsArrow and IsArrowRep ],
-#                IsIdenticalObj );
+# InstallMethod( \=,
+#                "for arrows",
+#  	       [ IsArrow and IsArrowRep, IsArrow and IsArrowRep ],
+# function( a1, a2 )
+#   return QuiverOfPath( a1 ) = QuiverOfPath( a2 )
+#          and ArrowNumber( a1 ) = ArrowNumber( a2 );
+# end );
+
+InstallMethod( \=, "for vertices",
+               [ IsVertex and IsVertexRep, IsVertex and IsVertexRep ],
+               IsIdenticalObj );
+
+InstallMethod( \=, "for arrows",
+               [ IsArrow and IsArrowRep, IsArrow and IsArrowRep ],
+               IsIdenticalObj );
 
 InstallMethod( \=,
                "for vertex and nontrivial path",

commit f6609857f44fec329aceb190d205f9e98a502009

Fixed a mistake in CoefficientsOfPathsSorted.
The mistake was introduced in commit 1ea55cb.

lib/algebra.gi
@@ -1081,6 +1081,10 @@ function( paths, e )
   j := Length( Ps );
   for i in [ 1 .. Length( paths ) ] do
     if j = 0 then
+      while i <= Length( paths ) do
+        coeffs[ i ] := zero;
+        i := i + 1;
+      od;
       break;
     fi;
     if paths[ i ] = Ps[ j ] then
@@ -1089,10 +1093,6 @@ function( paths, e )
     else
       coeffs[ i ] := zero;
     fi;
-  od;
-  while i <= Length( paths ) do
-    coeffs[ i ] := zero;
-    i := i + 1;
   od;
   return coeffs;
 end );