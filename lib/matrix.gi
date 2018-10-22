sunnyquiver/QPA2

commit 7a8e852182294a9fa8746f32b7b003fad256f0a0

Remove hidden dependency on the Polycyclic package
We had used the function IsIdentityMat without realizing that this
function is defined in the package Polycyclic, and not in GAP itself.
We could have added Polycyclic as a dependency in PackageInfo.g, but
since we don't really need that package for anything else, I have
instead rewritten the code to avoid calling IsIdentityMat.

Fixes #19

lib/matrix.gi
@@ -13,7 +13,7 @@ BindGlobal( "FamilyOfQPAMatrices",
 
 BindGlobal( "MAKE_QPA_MATRIX",
 function( R, M, type )
-  local dim, dim_, matrix;
+  local dim_, matrix, dim, is_identity_mat;
   dim_ := DimensionsMat( M );
   if type = IsRowMatrixRep then
     matrix := rec( rows := M );
@@ -24,7 +24,23 @@ function( R, M, type )
   else
     Error( "wrong type" );
   fi;
-  if IsIdentityMat( M ) then
+  is_identity_mat := function( mat )
+    local n, i, j;
+    n := DimensionsMat( mat )[ 1 ];
+    if DimensionsMat( mat )[ 2 ] <> n then
+      return false;
+    fi;
+    for i in [ 1 .. n ] do
+      for j in [ 1 .. n ] do
+        if i = j and not IsOne( mat[ i ][ j ] ) then
+          return false;
+        elif i <> j and not IsZero( mat[ i ][ j ] ) then
+          return false;
+        fi;
+      od;
+    od;
+  end;
+  if is_identity_mat( M ) then
     return IdentityMatrix( R, Length( M ) );
   elif M = NullMat( dim_[ 1 ], dim_[ 2 ], R ) then
     return MakeZeroMatrix( R, dim[ 1 ], dim[ 2 ] );

commit b5f914ced0d6c4d37d7094ef631f3f814ea951f8

Fix bug introduced in commit 7a8e8521
(In that commit, I added our own implementation of IsIdentityMat, but
forgot to include a "return true;" statement.)

lib/matrix.gi
@@ -39,6 +39,7 @@ function( R, M, type )
         fi;
       od;
     od;
+    return true;
   end;
   if is_identity_mat( M ) then
     return IdentityMatrix( R, Length( M ) );