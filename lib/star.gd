4b24c4ef5b3bb19748a08482d023310c83c55015
sunnyquiver <oyvind.solberg@ntnu.no>
2019-04-05 09:47:57 +0100

    Fix bug in documentation in star.gd

--- lib/star.gd
+++ lib/star.gd
@@ -6,21 +6,21 @@
 #!  This function computes the star of a module <Arg>M</Arg> for an algebra <M>A</M>, 
 #!  that is, <M>\Hom_A( M, A )</M>.
 #! @Returns <Ref Filt="IsQuiverModule"/>
-#! @Argurments M
+#! @Arguments M
 DeclareAttribute( "StarOfModule", IsQuiverModule );
 
 #! @Description
 #!  This function computes the star of a homomorphism between two modules <Arg>f</Arg> for an algebra <M>A</M>, 
 #!  that is, <M>\Hom_A( f, A )</M>.
 #! @Returns <Ref Filt="IsQuiverModuleHomomorphism"/>
-#! @Argurments f
+#! @Arguments f
 DeclareAttribute( "StarOfModuleHomomorphism", IsQuiverModuleHomomorphism );
 
 #! @Description
 #!  This function defines the star functor for a category of modules <Arg>C</Arg> 
 #!  for an algebra <M>A</M>, that is, <M>\Hom_A( -, A )\colon \mathcal{C}\to \mathcal{C}^\op</M>.
 #! @Returns <C>IsCapFunctor</C>
-#! @Argurments C
+#! @Arguments C
 DeclareOperation( "StarFunctor", [ IsQuiverModuleCategory ] );
 
 #
